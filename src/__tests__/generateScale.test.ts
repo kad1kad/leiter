159a76d24f55267f0ede137c5f21d76494b6060c
kad1kad <k.aliu@me.com>
2023-09-18 12:43:37 +0100

    add test for sharp tonic

--- src/__tests__/generateScale.test.ts
+++ src/__tests__/generateScale.test.ts
@@ -3,6 +3,13 @@ import { generateScale } from "../scales/generateScale";
 describe("generateScale function", () => {
   it("should increment the octave", () => {
     const scale = generateScale("A", 4, "major");
+    console.log("A4 major scale:", scale)
     expect(scale).toEqual(["A4", "B4", "C#5", "D5", "E5", "F#5", "G#5", "A5"]);
+  });
+
+  it("outputs sharp notes correctly using a sharp tonic note", () => {
+    const scale = generateScale("F#", 4, "major");
+    console.log("F#4 major scale:", scale)
+    expect(scale).toEqual(["F#4", "G#4", "A#4", "B4", "C#5", "D#5", "F5", "F#5"]);
   });
 });
